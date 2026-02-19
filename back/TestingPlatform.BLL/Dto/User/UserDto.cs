b94a3d914adf23d0b67a923934a39f0b2a904daa
SSkk999 <cvantymgeymshhhhh@gmail.com>
2026-02-19 19:34:24 +0200

    user-repo-dto

---
+++ back/TestingPlatform.BLL/Dto/User/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingPlatform.DAL.Entities;
+
+namespace TestingPlatform.BLL.Dto.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public Role Role { get; set; }
+    }
+}
