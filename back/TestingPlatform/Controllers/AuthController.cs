978c31df70cbe1ce2a6a23302c9e7ec7823710c5
SSkk999 <cvantymgeymshhhhh@gmail.com>
2026-02-21 10:57:50 +0200

    controller auth

---
+++ back/TestingPlatform/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestingPlatform.BLL.Dto.Auth;
+using TestingPlatform.BLL.Services.Auth;
+using TestingPlatform.Extensions;
+namespace TestingPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginDto dto)
+        {
+            var response = await _authService.LoginAsync(dto);
+            return this.ToActionResult(response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterDto dto)
+        {
+            var response = await _authService.RegisterAsync(dto);
+            return this.ToActionResult(response);
+        }
+    }
+}
