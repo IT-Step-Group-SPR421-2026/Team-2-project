3d56063d3c8f2b7aae4de85c77ec2311e5d278cf
SSkk999 <cvantymgeymshhhhh@gmail.com>
2026-03-04 18:57:34 +0200

    два контролера для відповідей

---
+++ back/TestingPlatform/Controllers/AnswerAttemptController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestingPlatform.BLL.Dto.AnswerAttempt;
+using TestingPlatform.BLL.Services.AnswerAttempt;
+using TestingPlatform.BLL.Services.Attempt;
+using TestingPlatform.Extensions;
+
+namespace TestingPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/answerattempt")]
+
+    public class AnswerAttemptController : Controller
+    {
+        private readonly IAnswerAttemptService _answerAttemptService;
+        private readonly IAttemptService _attemptService;
+
+        public AnswerAttemptController(IAnswerAttemptService answerAttemptService , IAttemptService attemptService)
+        {
+            _answerAttemptService = answerAttemptService;
+            _attemptService = attemptService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync(
+            [FromBody] CreateAnswerAttemptDto dto,
+            [FromQuery] string attemptId)
+        {
+            var response = await _answerAttemptService.CreateAsync(dto, attemptId);
+            return this.ToActionResult(response);
+        }
+
+        [HttpGet("by-id")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var response = await _answerAttemptService.GetByIdAsync(id);
+            return this.ToActionResult(response);
+        }
+
+        [HttpGet("by-attempt")]
+        public async Task<IActionResult> GetByAttemptIdAsync(string attemptId)
+        {
+            var response = await _attemptService.GetByIdAsync(attemptId);
+            return this.ToActionResult(response);
+        }
+    }
+}
---
+++ back/TestingPlatform/Controllers/AnswerOptionController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestingPlatform.BLL.Dto.AnswerOption;
+using TestingPlatform.BLL.Services.AnswerOption;
+using TestingPlatform.Extensions;
+
+namespace TestingPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/answeroption")]
+    public class AnswerOptionController : Controller
+    {
+        private readonly IAnswerOptionService _answerOptionService;
+
+        public AnswerOptionController(IAnswerOptionService answerOptionService)
+        {
+            _answerOptionService = answerOptionService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _answerOptionService.GetAllAsync();
+            return this.ToActionResult(response);
+        }
+
+
+        [HttpGet("by-question")]
+        public async Task<IActionResult> GetByQuestionIdAsync(string questionId)
+        {
+            var response = await _answerOptionService.GetByQuestionIdAsync(questionId);
+            return this.ToActionResult(response);
+        }
+
+
+        [HttpGet("by-id")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var response = await _answerOptionService.GetByIdAsync(id);
+            return this.ToActionResult(response);
+        }
+
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateAnswerOptionDto dto)
+        {
+            var response = await _answerOptionService.CreateAsync(dto);
+            return this.ToActionResult(response);
+        }
+
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateAsync([FromBody] AnswerOptionAdminDto dto)
+        {
+            var response = await _answerOptionService.UpdateAsync(dto);
+            return this.ToActionResult(response);
+        }
+
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteAsync(string id)
+        {
+            var response = await _answerOptionService.DeleteAsync(id);
+            return this.ToActionResult(response);
+        }
+
+    }
+}
+ 
